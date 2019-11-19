eda06657d5628d0bbb57695f6c9f5dd5566bb224
Catherine Barnett <ca.barnett06@gmail.com>
2019-11-18 19:05:26 -0500

    added function to update, past, present, and future

--- script.js
+++ script.js
@@ -40,3 +40,20 @@ $( document ).ready(function() {
     }
 formatAMPM();
 
+function updateColors(){
+    var currentTime = new Date().getHours();
+    for (var i = 9; i < 18; i++) { 
+    console.log(currentTime, $(`#${i}`).data("time"));
+     if ($(`#${i}`).data("time") == currentTime){
+        $(`#text${i}`).addClass( "present");
+    } else if (currentTime < $(`#${i}`).data("time")) {
+        $(`#text${i}`).addClass( "future");
+    }
+}
+}
+
+setInterval(function() {
+updateColors();
+}, 1000);
+
+
